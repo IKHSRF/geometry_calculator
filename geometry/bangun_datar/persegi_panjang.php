617d1564c5f248e8bd9e593f28b60981b7b0241f
rizkiansyahalfin <55535384+rizkiansyahalfin@users.noreply.github.com>
2021-02-10 21:49:15 +0700

    Update persegi_panjang.php

--- geometry/bangun_datar/persegi_panjang.php
+++ geometry/bangun_datar/persegi_panjang.php
@@ -13,10 +13,11 @@
         l = lebar
     </p>
 
-    <h2 align="center">Hitung Luas Persegi Panjang</h2>
+    <h2 align="center">Hitung Luas dan Keliling Persegi Panjang</h2>
     
     <h3>Isi Data:</h3>
-    <strong>Rumus: luas (L) = panjang (p) x lebar (l)</strong>
+    <strong>Rumus: luas (L) = panjang (p) x lebar (l)</strong><br>
+    <strong>Rumus: keliling (k) = 2 x (panjang + lebar) </strong>
     <form action="./" method="POST" enctype="multipart/form-data">
         <table>
             <tr>
@@ -32,76 +33,33 @@
             <tr>
                 <td></td>
                 <td></td>
-                <td><input type="submit" name="submit1" value="Hitung"></td>
+                <td><input type="submit" name="submit" value="Hitung"></td>
             </tr>
         </table>
     </form>
 
     <?php
-        if(isset($_POST['submit1'])){
+        if(isset($_POST['submit'])){
             $panjang = $_POST['p'];
             $lebar   = $_POST['l'];
             
-            // menghitung luas persegi panjang
+            // menghitung luas dan keliling persegi panjang
             $luas = $panjang * $lebar;
-
-            if ($luas == 0) {
-                echo "Ini bukan persegi panjang";
-            } else {
-                echo "Hasil hitung luas persegi panjang adalah sebagai berikut:<br />";
-                echo "Diketahui:<br />";
-                echo "panjang (p) = $panjang<br />";
-                echo "lebar (l) = $lebar<br />";
-                echo "Maka luas persegi panjang adalah L = panjang (p) x lebar (l) = $panjang x $lebar = $luas";
-            }
-        }
-    ?>
-
-
-    <h2 align="center">Hitung Keliling Persegi Panjang</h2>
-
-    <h3>Isi Data:</h3>
-    <strong>Rumus: keliling (k) = 2 x (panjang + lebar) </strong>
-    <form action="./" method="POST" enctype="multipart/form-data">
-        <table>
-            <tr>
-                <td>panjang (p)</td>
-                <td>:</td>
-                <td><input type="text" name="panjang" required></td>
-            </tr>
-            <tr>
-                <td>Lebar (l)</td>
-                <td>:</td>
-                <td><input type="text" name="lebar" required ></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td></td>
-                <td><input type="submit" name="submit2" value="Hitung"></td>
-            </tr>
-        </table>
-    </form>
-
-    <?php
-        if(isset($_POST['submit2'])){
-            $panjang = $_POST['panjang'];
-            $lebar   = $_POST['lebar'];
-            
-            // menghitung keliling persegi panjang
             $keliling = 2 * ($panjang + $lebar);
 
-            if ($keliling == 0) {
+
+            if ($luas && $luas == 0) {
                 echo "Ini bukan persegi panjang";
             } else {
-                echo "Hasil hitung keliling persegi panjang adalah sebagai berikut:<br />";
+                echo "Hasil hitung luas persegi panjang adalah sebagai berikut:<br/>";
                 echo "Diketahui:<br />";
-                echo "panjang (p) = $panjang<br />";
-                echo "lebar (l) = $lebar<br />";
-                echo "Maka keliling persegi panjang adalah K = 2 x (panjang + lebar) = 2 x ($panjang + $lebar) = $keliling";
+                echo "panjang (p) = $panjang<br/>";
+                echo "lebar (l) = $lebar<br/>";
+                echo "Maka luas persegi panjang adalah L = panjang (p) x lebar (l) = $panjang x $lebar = $luas<br/>";
+                echo "Dan keliling persegi panjang adalah K = 2 x (panjang + lebar) = 2 x ($panjang + $lebar) = $keliling";
             }
         }
     ?>
-
 
 </body>
 </html>
