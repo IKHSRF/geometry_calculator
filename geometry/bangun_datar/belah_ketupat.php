48e8fe613166951923ca155b2f2b9a1a4c84fa6b
rizkiansyahalfin <55535384+rizkiansyahalfin@users.noreply.github.com>
2021-02-10 21:52:59 +0700

    Update belah_ketupat.php

--- geometry/bangun_datar/belah_ketupat.php
+++ geometry/bangun_datar/belah_ketupat.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Bangun Datar | Belah Ketupat</title>
+    <title>Bangun Datar | Layang-layang</title>
 </head>
 <body>
-    <h1 align="center">Belah Ketupat</h1>
-    <p>Belah Ketupat adalah bangun datar 2 dimensi yang dibentuk oleh 4 buah sisi yang sama panjang dan mempunyai 2 pasang sudut bukan siku-siku dengan sudut yang saling berhadapan mempunyai besar sama.</p>
-    <img src="belah Ketupat.png" alt="belah Ketupat">
+    <h1 align="center">Layang-layang</h1>
+    <p>Layang-layang adalah bangun datar 2 dimensi yang dibentuk oleh 4 buah sisi yang sama panjang dan mempunyai 2 pasang sudut bukan siku-siku dengan sudut yang saling berhadapan mempunyai besar sama.</p>
+    <img src="layang-layang.png" alt="layang-layang">
     <strong>Keterangan:</strong>
     <p>
-        s = sisi belah ketupat <br>
+        a, b, c, d = sisi layang-layang <br>
         d1, d2 = diagonal
     </p>
 
-    <h2 align="center">Hitung Luas Belah Ketupat</h2>
+    <h2 align="center">Hitung Luas Layang-layang</h2>
     <strong>Rumus: luas (L) = &#189; x diagonal 1 x diagonal 2</strong>
 
     <h3>Isi Data:</h3>
@@ -42,32 +42,38 @@
             $diagonal_1 = $_POST['d1'];
             $diagonal_2 = $_POST['d2'];
             
-            // menghitung luas belah ketupat
+            // menghitung luas layang-layang
             $luas = 1/2 * $diagonal_1 * $diagonal_2;
 
             if ($luas == 0) {
-                echo "Ini bukan belah ketupat";
+                echo "Ini bukan layang-layang";
             } else {
-                echo "Hasil hitung luas belah ketupat adalah sebagai berikut:<br />";
+                echo "Hasil hitung luas layang-layang adalah sebagai berikut:<br />";
                 echo "Diketahui:<br/>";
-                echo "diagonal (d1) = $diagonal_1<br/>";
-                echo "diagonal (d2) = $diagonal_2<br/>";
-                echo "Maka luas belah ketupat adalah L = &#189; x diagonal (d1) x diagonal (d2) = $diagonal_1 x $diagonal_2 = $luas";
+                echo "diagonal 1 = $diagonal_1<br/>";
+                echo "diagonal 2 = $diagonal_2<br/>";
+                echo "Maka luas layang-layang adalah L = &#189; x diagonal 1 x diagonal 2 = $diagonal_1 x $diagonal_2 = $luas";
             }
         }
     ?>
 
-    <h2 align="center">Hitung Keliling Belah Ketupat</h2>
-    <strong>Rumus: keliling (k) = 4 x sisi</strong>
+    <h2 align="center">Hitung Keliling layang-layang</h2>
+    <strong>Rumus: keliling (k) = 2 x (a + c)</strong>
 
     <h3>Isi Data:</h3>
     <form action="./" method="POST" enctype="multipart/form-data">
         <table>
             <tr>
-                <td>sisi (s)</td>
+                <td>sisi (a)</td>
                 <td>:</td>
-                <td><input type="text" name="sisi" required></td>
+                <td><input type="text" name="sisi_a" required></td>
             </tr>
+            <tr>
+                <td>sisi (c)</td>
+                <td>:</td>
+                <td><input type="text" name="sisi_c" required></td>
+            </tr>
+
             <tr>
                 <td></td>
                 <td></td>
@@ -77,18 +83,20 @@
     </form>
     <?php
         if(isset($_POST['submit2'])){
-            $sisi = $_POST['sisi'];
+            $sisi_a = $_POST['sisi_a'];
+            $sisi_c = $_POST['sisi_c'];
             
-            // menghitung keliling belah ketupat
-            $keliling = 4 * $sisi;
+            // menghitung keliling layang-layang
+            $keliling = 2 * ($sisi_a + $sisi_c);
 
             if ($keliling == 0) {
-                echo "Ini bukan belah ketupat";
+                echo "Ini bukan layang-layang";
             } else {
-                echo "Hasil hitung keliling belah ketupat adalah sebagai berikut:<br/>";
+                echo "Hasil hitung keliling layang-layang adalah sebagai berikut:<br />";
                 echo "Diketahui:<br/>";
-                echo "sisi (s) = $sisi<br/>";
-                echo "Maka keliling belah ketupat adalah K = 4 x sisi (s) = 4 x $sisi = $keliling";
+                echo "sisi (a) = $sisi_a<br/>";
+                echo "sisi (c) = $sisi_c<br/>";
+                echo "Maka keliling layang-layang adalah K = 2 x (sisi a + sisi c) = 2 x ($sisi_a + $sisi_c) = $keliling";
             }
         }
     ?>
