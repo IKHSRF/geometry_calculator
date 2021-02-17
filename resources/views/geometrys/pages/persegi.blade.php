013d84cf095e065d0f5b3579373a734950475140
Ikhsan Arfian Nugraha <ikhsan3568@gmail.com>
2021-02-17 09:45:55 +0700

    edit tampilan untuk persegi dan persegi panjang

--- resources/views/geometrys/pages/persegi.blade.php
+++ resources/views/geometrys/pages/persegi.blade.php
@@ -1,68 +1,96 @@
 @extends('geometrys.layout')
  
-@section('content')
-<div class="container">
-    <!-- menghitung luas persegi -->
-    <div class="row justify-content-around">
-        <div class="card pr-2" style="">
-            <div class="card-body">
-                <a href="/geometri" class="btn btn-primary mb-3" align="center">Kembali</a>
-                <h3>Persegi</h3>
-                <p>Persegi adalah bangun datar 2 dimensi yang dibentuk oleh empat sisi yang sama panjang dan keempat titik sudutnya membentuk sudut siku-siku (90º).</p>
-                <div class="row">
-                    <div class="col-sm">
-                        <strong>Rumus:</strong><br>
-                        <i>luas (L) = sisi (s) x sisi (s)</i><br>
-                        <i>keliling (k) = sisi (s) x 4</i>
-                    </div>
-                    <div class="col-sm">
-                        <strong>Keterangan: </strong><br>
-                        <i>s = sisi persegi</i>
-                    </div>
-                 </div>
-                <br>
-                <h3>Luas dan Keliling Persegi</h3>
-                <div class="row">
-                    <div class="col-sm">
-                        <table>
-                            <tr>
-                                <td><label for="s"><i>sisi (s)</i></label></td>
-                                <td>:</td>
-                                <td><input type="number" id="s" required min="1" minlength="1" maxlength="21"></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td><button onclick="hitung()" class="btn btn-primary mt-3">Hitung</button></td>
-                            </tr>
-                        </table>
-                    </div>
-                    <div class="col-sm">
-                    <table>
-                            <tr>
-                                <td><label for="luas"><i>luas (L)</i></label></i></td>
-                                <td>:</td>
-                                <td><input type="text" id="luas" disabled readonly></td>
-                            </tr>
-                            <tr>
-                                <td><label for="keliling"><i>keliling (k))</i></label><i></i></td>
-                                <td>:</td>
-                                <td><input type="text" id="keliling" disabled readonly></td>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
-                <script type="text/javascript">
-                    var sisi = document.getElementById('s')
-                    var luas = document.getElementById('luas')
-                    var keliling = document.getElementById('keliling')
-                    function hitung() {
-                        luas.value = Number(sisi.value) * Number(sisi.value)
-                        keliling.value = Number(sisi.value) * 4
-                    }
-                </script>
-            </div>
+@section('header')
+  <section id="hero" class="d-flex align-items-center">
+    <div
+        class="container position-relative"
+        data-aos="fade-up"
+        data-aos-delay="100">
+      <a href="/geometri" class="btn btn-primary mb-3" align="center">Kembali</a>
+      <div class="row justify-content-center">
+        
+        <div class="col-xl-7 col-lg-9 text-center">
+          <h1>Persegi</h1>
+          <h2>
+            Persegi adalah bangun datar 2 dimensi yang dibentuk oleh empat
+            sisi yang sama panjang dan keempat titik sudutnya membentuk sudut
+            siku-siku (90º).
+          </h2>
         </div>
+      </div>
     </div>
-</div>
+  </section>
+@endsection
+
+
+@section('content')
+<main id="main">
+      <!-- ======= Contact Section ======= -->
+      <section id="contact" class="contact">
+        <div class="container" data-aos="fade-up">
+          <div class="section-title">
+            <div class="row">
+              <div class="col-sm">
+                <strong>Rumus:</strong><br />
+                <i>luas (L) = sisi (s) x sisi (s)</i><br />
+                <i>keliling (k) = sisi (s) x 4</i>
+              </div>
+              <div class="col-sm">
+                <strong>Keterangan: </strong><br />
+                <i>s = sisi persegi</i>
+              </div>
+            </div>
+          </div>
+          <div class="row mt-5 d-flex justify-content-center">
+            <div class="col-lg-8 mt-5 mt-lg-0">
+              <div class="form-group">
+                <input
+                  type="text"
+                  class="form-control"
+                  name="Sisi"
+                  id="s"
+                  placeholder="Sisi"
+                />
+              </div>
+              <div class="text-center">
+                <button onclick="hitung()" class="btn btn-primary">
+                  Hitung
+                </button>
+              </div>
+              <div class="form-row d-flex justify-content-center">
+                <div class="col-lg-4">
+                  <div class="info">
+                    <div class="email">
+                      <i class="icofont-envelope"></i>
+                      <h4>Keliling</h4>
+                      <p id="keliling"></p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-lg-4">
+                  <div class="info">
+                    <div class="email">
+                      <i class="icofont-envelope"></i>
+                      <h4>Luas</h4>
+                      <p id="luas"></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- End Contact Section -->
+    </main>
+    <script type="text/javascript">
+      var sisi = document.getElementById("s");
+      var luas = document.getElementById("luas");
+      var keliling = document.getElementById("keliling");
+      function hitung() {
+        luas.textContent = Number(sisi.value) * Number(sisi.value);
+        keliling.textContent = Number(sisi.value) * 4;
+      }
+    </script>
 @endsection
--- resources/views/geometrys/pages/persegipanjang.blade.php
+++ resources/views/geometrys/pages/persegipanjang.blade.php
@@ -1,77 +1,101 @@
-@extends('geometrys.layout')
- 
-@section('content')
-<div class="container">
-    <!-- menghitung luas persegi -->
-    <div class="row justify-content-around">
-        <div class="card pr-2" style="">
-            <div class="card-body">
-                <a href="/geometri" class="btn btn-primary mb-3" align="center">Kembali</a>
-                <h3>Persegi Panjang</h3>
-                <p>Persegi panjang adalah bangun datar 2 dimensi yang mempunyai 2 pasang sisi sejajar yang sama Panjang dan mempunyai 4 titik sudut siku-siku.</p>
-                <div class="row">
-                    <div class="col-sm">
-                        <strong>Rumus:</strong><br>
-                        <i>luas (L) = panjang (p) x lebar (l)</i><br>
-                        <i>keliling (k) = 2 x (panjang + lebar)</i>
-                    </div>
-                    <div class="col-sm">
-                        <strong>Keterangan:</strong><br>
-                        <i>
-                            p = panjang <br>
-                            l = lebar
-                        </i>
-                    </div>
-                </div>
-                <br>
-                <h3>Luas dan Keliling Persegi Panjang</h3>
-                <div class="row">
-                    <div class="col-sm">
-                        <table>
-                            <tr>
-                                <td><label for="p"><i>panjang (p)</i></label></td>
-                                <td>:</td>
-                                <td><input type="number" id="p" required min="1" minlength="1" maxlength="21"></td>
-                            </tr>
-                            <tr>
-                                <td><label for="l"><i>lebar (l)</i></label></td>
-                                <td>:</td>
-                                <td><input type="number" id="l" required min="1" minlength="1" maxlength="21"></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td><button onclick="hitung()" class="btn btn-primary mt-3">Hitung</button></td>
-                            </tr>
-                        </table>
-                    </div>
-                    <div class="col-sm">
-                        <table>
-                            <tr>
-                                <td><label for="luas"><i>luas (L)</i></label></td>
-                                <td>:</td>
-                                <td><input type="text" id="luas" disabled readonly></td>
-                            </tr>
-                            <tr>
-                                <td><label for="keliling"><i>keliling (k)</i></label></td>
-                                <td>:</td>
-                                <td><input type="text" id="keliling" disabled readonly></td>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
-                <script type="text/javascript">
-                    var	panjang = document.getElementById('p')
-                    var lebar = document.getElementById('l')
-                    var	luas = document.getElementById('luas')
-                    var	keliling =  document.getElementById('keliling')
-                    function hitung() {
-                        luas.value = Number(panjang.value) * Number(lebar.value)
-                        keliling.value = 2 * (Number(panjang.value) + Number(lebar.value))
-                    }
-                </script>
-            </div>
-        </div>
+@extends('geometrys.layout') @section('header')
+<section id="hero" class="d-flex align-items-center">
+  <div
+    class="container position-relative"
+    data-aos="fade-up"
+    data-aos-delay="100"
+  >
+    <a href="/geometri" class="btn btn-primary mb-3" align="center">Kembali</a>
+    <div class="row justify-content-center">
+      <div class="col-xl-7 col-lg-9 text-center">
+        <h1>Persegi Panjang</h1>
+        <h2>
+          Persegi panjang adalah bangun datar 2 dimensi yang mempunyai 2 pasang
+          sisi sejajar yang sama Panjang dan mempunyai 4 titik sudut siku-siku.
+        </h2>
+      </div>
     </div>
-</div>
+  </div>
+</section>
+@endsection @section('content')
+<main id="main">
+  <!-- ======= Contact Section ======= -->
+  <section id="contact" class="contact">
+    <div class="container" data-aos="fade-up">
+      <div class="section-title">
+        <div class="row">
+          <div class="col-sm">
+            <strong>Rumus:</strong><br />
+            <i>luas (L) = panjang (p) x lebar (l)</i><br />
+            <i>keliling (k) = 2 x (panjang + lebar)</i>
+          </div>
+          <div class="col-sm">
+            <strong>Keterangan: </strong><br />
+            <i>
+              p = panjang <br />
+              l = lebar
+            </i>
+          </div>
+        </div>
+      </div>
+      <div class="row mt-5 d-flex justify-content-center">
+        <div class="col-lg-8 mt-5 mt-lg-0">
+          <div class="form-group">
+            <input
+              type="text"
+              class="form-control"
+              name="Panjang"
+              id="p"
+              placeholder="Panjang"
+            />
+          </div>
+          <div class="form-group">
+            <input
+              type="text"
+              class="form-control"
+              name="Lebar"
+              id="l"
+              placeholder="Lebar"
+            />
+          </div>
+          <div class="text-center">
+            <button onclick="hitung()" class="btn btn-primary">Hitung</button>
+          </div>
+          <div class="form-row d-flex justify-content-center">
+            <div class="col-lg-4">
+              <div class="info">
+                <div class="email">
+                  <i class="icofont-envelope"></i>
+                  <h4>Keliling</h4>
+                  <p id="keliling"></p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-lg-4">
+              <div class="info">
+                <div class="email">
+                  <i class="icofont-envelope"></i>
+                  <h4>Luas</h4>
+                  <p id="luas"></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Contact Section -->
+</main>
+<script type="text/javascript">
+  var panjang = document.getElementById("p");
+  var lebar = document.getElementById("l");
+  var luas = document.getElementById("luas");
+  var keliling = document.getElementById("keliling");
+  function hitung() {
+    luas.textContent = Number(panjang.value) * Number(lebar.value);
+    keliling.textContent = 2 * (Number(panjang.value) + Number(lebar.value));
+  }
+</script>
 @endsection
